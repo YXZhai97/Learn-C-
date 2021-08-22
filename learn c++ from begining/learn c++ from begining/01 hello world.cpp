79e834fad051f81bf28c74c2c61dbb5f5211bc34
Eason <eason.zhai97@outlook.com>
2021-08-22 21:14:37 +0200

    learn cpp

---
+++ learn c++ from begining/learn c++ from begining/01 hello world.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//print out "hello world" to output
+	/*the
+	same 
+	*/
+	cout << "hello world" << endl;
+	system("pause");
+
+	return 0;
+}
---
+++ learn c++ from begining/learn c++ from begining/04 constant.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+#define Day 7
+
+int main()
+{
+	const int month = 12;
+	cout << "One week has " << Day  << " days" << endl;
+	cout << "one year has " << month  << " months" << endl;
+	system("pause");
+	return 0;
+
+
+}
